fdb4ddd91cb0d04f01574031a66c7b103cb13673
tduretz <duretz@uni-frankfurt.de>
2025-06-11 11:38:44 +0200

    merge

--- src/inside.jl
+++ src/inside.jl
@@ -1,20 +1,14 @@
 using GridGeometryUtils
 
 function inside(p::Point, rect::Rectangle)
-
     (; origin, h, l,  cosθ, sinθ) = rect
 
-    x_temp = [p[1]-origin[1], p[2]-origin[2]] # TODO: remove once all inputs are SArrays
-
     # Shift
-    𝐱 = SVector{2}(x_temp)
-
+    𝐱 = p - origin
     # Rotation matrix
     𝐑  = @SMatrix([ cosθ -sinθ; sinθ cosθ])
-
     # Rotate geometry
     𝐱′ = 𝐑*𝐱
-
     # Check if inside
     return abs(𝐱′[1]) ≤ l/2 && abs(𝐱′[2]) ≤ h/2
 end
